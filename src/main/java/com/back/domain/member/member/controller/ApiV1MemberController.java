8461bad7e1fbabd08321e538466037a02a4fc834
EuniceNam <caellime@gmail.com>
2026-06-15 09:44:52 +0900

    test:MemberControllerTest 환경

---
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,22 @@
+package com.back.domain.member.member.controller;
+
+
+import com.back.domain.member.member.dto.MemberDto;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+@Tag(name = "ApiV1MemberController", description = "API 회원 컨트롤러")
+public class ApiV1MemberController {
+    private final MemberService memberService;
+
+}
