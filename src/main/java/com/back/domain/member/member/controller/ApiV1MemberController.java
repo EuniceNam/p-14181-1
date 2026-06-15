685bcb0a1df8c4f633d02aac2acc96e52de62650
EuniceNam <caellime@gmail.com>
2026-06-15 09:45:39 +0900

    study: 안 보고 해보기(red)

--- src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -19,4 +19,10 @@ import org.springframework.web.client.RestClient;
 public class ApiV1MemberController {
     private final MemberService memberService;
 
+    @PostMapping("/join")
+    public RsData<MemberDto> join(Member member) {
+        Member res = memberService.join(member.getUsername(), member.getPassword(), member.getNickname());
+
+        return new RsData<MemberDto>("201-1", "%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(res.getName()), new MemberDto(res));
+    }
 }
