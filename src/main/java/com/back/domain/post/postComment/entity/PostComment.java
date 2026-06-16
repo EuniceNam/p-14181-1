1c5c21e97104a620151531ddcd09654a8bed8b77
EuniceNam <caellime@gmail.com>
2026-06-16 10:31:29 +0900

    test: authorization 헤더 관련 테스트케이스 추가

--- src/main/java/com/back/domain/post/postComment/entity/PostComment.java
+++ src/main/java/com/back/domain/post/postComment/entity/PostComment.java
@@ -33,7 +33,7 @@ public class PostComment extends BaseEntity {
 
     public void checkActorCanModify(Member actor) {
         if (!author.equals(actor))
-            throw new ServiceException("403-1", "댓글의 작성자만 수정할 수 있습니다.");
+            throw new ServiceException("403-1", "%d번 댓글 수정권한이 없습니다.".formatted(getId()));
     }
 
     public void checkActorCanDelete(Member actor) {
